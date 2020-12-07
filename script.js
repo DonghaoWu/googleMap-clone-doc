c103e3ac057398c5af6fc300d6dca826860d983a
Donghao Wu <donghao.wu.us@gmail.com>
2020-12-07 17:21:36 -0500

    fixed some naming bugs.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZG9uZ2hhbyIsImEiOiJjamdzZzBhZm8wZ3hkMnFxOWdndm4zcXFvIn0.lIbs9i7j31tbOfR_EpTC2g';
 
 const successLocation = (position) => {
-    setupMap([position.coords.longtitude, position.coords.latitude]);
+    setupMap([position.coords.longitude, position.coords.latitude]);
 }
 
 const errorLocation = () => {
